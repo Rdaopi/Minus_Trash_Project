7de556fc2ae77200e3f812356d507fe5cd7f8c31
Davide Wu <wudavide03@gmail.com>
2025-05-08 13:32:48 +0200

    da modificare path dei routes nel swagger.js

--- Server/config/swagger.js
+++ Server/config/swagger.js
@@ -1,15 +1,15 @@
 // server/config/swagger.js
-import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerJsdoc from 'swagger-jsdoc'; // Importa la libreria per generare la documentazione Swagger/OpenAPI.
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: '3.0.0', //versione
     info: {
-      title: 'Minus Trash API',
+      title: 'Minus Trash API', //nome API
       version: '1.0.0',
       description: 'API per la gestione rifiuti smart'
     },
-    servers: [{ url: 'http://localhost:3000/api' }],
+    servers: [{ url: 'http://localhost:3000/api' }],     // manca il URL quello vero
     components: {
       securitySchemes: {
         BearerAuth: {
@@ -20,7 +20,7 @@ const options = {
       }
     }
   },
-  apis: ['./src/modules/*/routes.js']
+  apis: ['./src/modules/*/routes.js'] //Specifica i file contenenti le annotazioni Swagger (JSDoc). da modificare 
 };
 
 export default swaggerJsdoc(options);   
