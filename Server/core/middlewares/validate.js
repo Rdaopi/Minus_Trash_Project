cb66794af2f2a030a4a6114f5e757d164c10c80a
Riccardo Guglielmi <62119079+Rdaopi@users.noreply.github.com>
2025-05-12 17:55:54 +0200

    Delete Server/core/middlewares directory
    Cancellazione cartella a seguito del merge con backend

--- Server/core/middlewares/validate.js
+++
@@ -1,7 +0,0 @@
-// validate.js
-import { body } from 'express-validator';
-
-export const registerValidator = [
-  body('email').isEmail().normalizeEmail(),
-  body('password').isStrongPassword()
-];
